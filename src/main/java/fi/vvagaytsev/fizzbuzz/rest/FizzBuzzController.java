fea4129aec70d5627cc8a88da9e6f67d9ae32a85
vvagaytsev <vvagaytsev@alpha-sense.com>
2018-08-09 10:03:39 +0300

    Basic Swagger config

--- src/main/java/fi/vvagaytsev/fizzbuzz/rest/FizzBuzzController.java
+++ src/main/java/fi/vvagaytsev/fizzbuzz/rest/FizzBuzzController.java
@@ -1,6 +1,7 @@
 package fi.vvagaytsev.fizzbuzz.rest;
 
 import fi.vvagaytsev.fizzbuzz.service.FizzBuzzService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import java.util.List;
  * @author Vladimir Vagaytsev
  * Date: 09/08/2018
  */
+@Api(description = "FizzBuzz API")
 @RestController
 @RequestMapping("/api")
 public class FizzBuzzController {
