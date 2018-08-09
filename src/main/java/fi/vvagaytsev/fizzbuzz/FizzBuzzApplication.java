094e10612523289a5e937af6ccc284c3f941183d
vvagaytsev <vvagaytsev@alpha-sense.com>
2018-08-09 10:15:04 +0300

    Added request logging filter

--- src/main/java/fi/vvagaytsev/fizzbuzz/FizzBuzzApplication.java
+++ src/main/java/fi/vvagaytsev/fizzbuzz/FizzBuzzApplication.java
@@ -2,6 +2,8 @@ package fi.vvagaytsev.fizzbuzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
  * @author Vladimir Vagaytsev
@@ -12,5 +14,17 @@ public class FizzBuzzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FizzBuzzApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(1000);
+        filter.setIncludeHeaders(false);
+        filter.setBeforeMessagePrefix("INCOMING REQUEST : ");
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
     }
 }
